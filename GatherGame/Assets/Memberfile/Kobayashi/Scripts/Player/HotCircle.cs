71d737cd12e8a9022ee11e8145d6cdabdff79007
SenaChan <69202734+SenaChan@users.noreply.github.com>
2020-09-05 18:10:03 +0900

    プレイヤーの移動、サークルの仕組み、カメラの追従、見下ろし視点を作成しました。 また、適当に探した無料アセットを使って地面を氷っぽくしました。

---
+++ GatherGame/Assets/Memberfile/Kobayashi/Scripts/Player/HotCircle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotCircle : MonoBehaviour
+{
+
+    private GameObject parent;
+    private PlayerMove playerMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //親オブジェクトを取得
+        parent = transform.root.gameObject;
+
+        //親オブジェクトがPlayerならばMoveを取得
+        if(parent == GameObject.FindGameObjectWithTag("Player"))
+        {
+           playerMove = parent.GetComponent<PlayerMove>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Circle")
+            if (parent == GameObject.FindGameObjectWithTag("Player"))
+                playerMove.enabled = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Circle")
+            if (parent == GameObject.FindGameObjectWithTag("Player"))
+                playerMove.enabled = false;
+    }
+}
