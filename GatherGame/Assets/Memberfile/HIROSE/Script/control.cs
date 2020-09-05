224a6de775eab34fa5996fcd1347d91aaa194101
1823063 <hirosesin@gmail.com>
2020-09-05 20:33:27 +0900

    ゲーム制作(滑る床作成中)

--- GatherGame/Assets/Memberfile/HIROSE/Script/control.cs
+++ GatherGame/Assets/Memberfile/HIROSE/Script/control.cs
@@ -11,7 +11,6 @@ public class control : MonoBehaviour
     public float LRspeed = 3.0f;
     public float span = 3f;//間隔
     public float currentTime;//時間計測
-    public float CharacterMotor;
     // Start is called before the first frame update
     void Start()
     {
