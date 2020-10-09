a268c84e0abfe028534438fa640ce91a2be6a3f1
SenaChan <69202734+SenaChan@users.noreply.github.com>
2020-10-09 17:29:12 +0900

    no message

---
+++ GatherGame/Assets/Memberfile/Kobayashi/Scripts/Player/PlayerMoveDPad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveDPad : MonoBehaviour
+{
+
+    Rigidbody rb;
+    [SerializeField] private float speed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("DS4DHorizontal");
+        float z = Input.GetAxis("DS4DVertical");
+        //rb.AddForce(x * speed * Time.deltaTime, 0, z * speed * Time.deltaTime);
+
+        Vector3 Ppos = transform.position;
+        Ppos += new Vector3(x * speed * Time.deltaTime, 0, z * speed * Time.deltaTime);
+        transform.position = Ppos;
+    }
+}
