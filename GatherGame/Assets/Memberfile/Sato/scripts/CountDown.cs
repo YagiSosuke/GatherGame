fb96800e236038e4b9d2679c4e2d67c3bae4f8e2
yagikun <yagikundayo@gmail.com>
2020-12-11 21:08:57 +0900

    ファイル内容を最新(佐藤君の編集内容)に更新

---
+++ GatherGame/Assets/Memberfile/Sato/scripts/CountDown.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+
+public class CountDown : MonoBehaviourPunCallbacks
+{
+    public Text timerText;
+
+    public static float totalTime;
+    public float ResetTime;
+    int seconds;
+
+    private GameObject parent;
+
+    // Use this for initialization
+    void Start()
+    {
+        parent = GameObject.Find("Penguin(Clone)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            if (HotCircles.CountDown == true)
+            {
+                totalTime -= Time.deltaTime;
+                seconds = (int)totalTime;
+                timerText.text = "GameOverまで" + "\n\n" + seconds.ToString();
+            }
+            else
+            {
+                totalTime = ResetTime;
+                timerText.text = " ";
+            }
+
+            if (totalTime <= 0)
+            {
+                parent.gameObject.SetActive(false);
+            }
+            if (totalTime <0)
+            {
+                SceneManager.LoadScene("Risalt");
+            }
+        }
+    }
+}
