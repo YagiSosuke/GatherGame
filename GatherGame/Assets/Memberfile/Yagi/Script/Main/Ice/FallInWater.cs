95bfdb826ed0c37c15d4eeba188ecbf0c98c4823
yagikun <yagikundayo@gmail.com>
2021-02-10 16:22:55 +0900

    ゲーム画面 - 割れる床、途中

---
+++ GatherGame/Assets/Memberfile/Yagi/Script/Main/Ice/FallInWater.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*水に落ちる*/
+
+public class FallInWater : MonoBehaviour
+{
+    Rigidbody ObjRB;
+    AudioSource audio;
+    [SerializeField]AudioClip DamageSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjRB = gameObject.transform.parent.GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Water")
+        {
+            //吹っ飛ぶ方向
+            Vector3 moveVec = (transform.position - col.transform.position).normalized * 3000;
+            moveVec = new Vector3(moveVec.x, 0, moveVec.z);
+            ObjRB.AddForce(moveVec);
+            audio.PlayOneShot(DamageSound);
+
+            Invoke("VelocityStop", 0.2f);
+        }
+    }
+
+    //停止する
+    void VelocityStop()
+    {
+        ObjRB.velocity = Vector3.zero;
+    }
+}
---
+++ GatherGame/Assets/Memberfile/Yagi/Script/Main/Ice/IceBreak.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*氷が割れる*/
+
+public class IceBreak : MonoBehaviour
+{
+    bool BreakF = false;
+    float Scale_x = 595;
+    float count = 0;
+
+    Vector3 BeforeScale;
+    Vector3 AfterScale;
+
+    //氷のオブジェクト
+    [SerializeField] GameObject Ice;
+    [SerializeField] GameObject Hole;
+
+    //割れる効果音
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BeforeScale = new Vector3(0, 297, 1605);
+        AfterScale = new Vector3(Scale_x, 297, 1605);
+
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BreakF)
+        {
+            Ice.transform.localScale = Vector3.Lerp(BeforeScale, AfterScale, count);
+
+            if (count < 1.0f)
+            {
+                count += Time.deltaTime * 5.0f;
+            }
+            else
+            {
+                count = 1.0f;
+            }
+        }
+    }
+    
+    void OnTriggerEnter(Collider col)
+    {
+        //プレイヤーが氷が割れる範囲に侵入したとき
+        if(col.tag == "Player")
+        {
+            if (!BreakF)
+            {
+                BreakF = true;
+                Hole.SetActive(true);
+                audio.Play();
+            }
+        }
+    }
+}
