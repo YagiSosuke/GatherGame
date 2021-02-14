acb6fc69c0bfc354d43fb89994f19722d17c5fe3
yagikun <yagikundayo@gmail.com>
2021-02-14 12:33:44 +0900

    ゲーム画面 - 自動で落とし穴を生成するようにした

---
+++ GatherGame/Assets/Memberfile/Yagi/Script/Main/IceBreakCreate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*落とし穴を自動生成する*/
+
+public class IceBreakCreate : MonoBehaviour
+{
+    [SerializeField] int IceBreakMAX;
+    public int IceBreakCount = 0;
+
+    [SerializeField] GameObject IceBreak;
+    GameObject instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while(IceBreakCount < IceBreakMAX)
+        {
+            instance = Instantiate(IceBreak);
+            instance.transform.position = new Vector3(Random.Range(-400, 400), 0, Random.Range(-400, 400));
+            instance.transform.Rotate(0, Random.Range(0,360), 0);
+            instance.transform.SetParent(this.gameObject.transform);
+
+            IceBreakCount++;
+        }
+    }
+}
