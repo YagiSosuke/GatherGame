954b0074ff862ae16ad3e0e2d8bd8f7e257c9af7
yagikun <yagikundayo@gmail.com>
2021-02-23 15:10:26 +0900

    ゲーム画面 - 吹雪途中

---
+++ GatherGame/Assets/Memberfile/Yagi/Script/Main/Wind/BlowWind.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*風が吹いた時にペンギンを動かす*/
+
+public class BlowWind : MonoBehaviour
+{
+    Rigidbody rb;
+    WindController windScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        windScript = GameObject.Find("WindObject").GetComponent<WindController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (windScript.WindF)
+        {
+            if (windScript.count == 0)
+            {
+                rb.AddForce(rb.velocity + windScript.WindAngle * windScript.WindPower);
+                //rb.AddForce(rb.velocity + new Vector3(0, 0, 300f));
+            }
+        }
+        else
+        {
+            if (windScript.count == 0)
+            {
+                rb.velocity = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ GatherGame/Assets/Memberfile/Yagi/Script/Main/Wind/WindController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindController : MonoBehaviour
+{
+    //風が吹く間隔
+    [SerializeField] float intervalMAX = 20;
+    [SerializeField] float intervalMIN = 10;
+    [SerializeField] float interval;
+    public float count = 0;
+
+    //風が吹く時間
+    public float WindTime = 3;
+
+    //風の向き
+    public Vector3 WindAngle;
+
+    //風力
+    public float WindPower = 300;
+
+    //効果音
+    AudioSource audio;
+    [SerializeField] AudioClip windAudio;
+
+    public bool WindF = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        WindSetting();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count += Time.deltaTime;
+
+        if (!WindF)
+        {
+            if (interval < count)
+            {
+                WindF = true;
+                audio.PlayOneShot(windAudio);
+                count = 0;
+            }
+        }
+        else
+        {
+            if(WindTime < count)
+            {
+                WindF = false;
+                WindSetting();
+            }
+        }
+    }
+
+    //風の設定
+    void WindSetting()
+    {
+        count = 0;
+        interval = Random.Range(intervalMIN, intervalMAX);
+
+        float angle = Random.Range(0, 360);
+        float rad = angle * Mathf.Deg2Rad;
+        float px = Mathf.Cos(rad);
+        float pz = Mathf.Sin(rad);
+        WindAngle = new Vector3(px, 0, pz);
+
+    }
+}
