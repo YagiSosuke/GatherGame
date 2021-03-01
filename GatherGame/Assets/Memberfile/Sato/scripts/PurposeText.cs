1f7f808a53d0aa036c4dc71f89bc0b1c2a26bdd0
SenaChan <69202734+SenaChan@users.noreply.github.com>
2021-03-02 02:03:14 +0900

    各シーン細かい調整をしました

---
+++ GatherGame/Assets/Memberfile/Sato/scripts/PurposeText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+
+public class PurposeText : MonoBehaviourPunCallbacks
+{
+    public Text purposeText;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (photonView.IsMine)
+        //{
+            if (!HotCircles.CountDown)
+                purposeText.text = "無事に群れに帰ろう!";
+            else
+                purposeText.text = "急いで助けてもらおう!";
+        //}
+    }
+}
