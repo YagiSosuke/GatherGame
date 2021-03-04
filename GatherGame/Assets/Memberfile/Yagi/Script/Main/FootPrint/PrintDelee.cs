3b6122b777f59ba7ec01d491198b762d3335bfc0
yagikun <yagikundayo@gmail.com>
2021-03-04 23:52:17 +0900

    足跡途中

---
+++ GatherGame/Assets/Memberfile/Yagi/Script/Main/FootPrint/PrintDelee.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*時間経過で足跡を消す*/
+
+public class PrintDelee : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer foot;
+
+    float time = 0;
+    //残る時間
+    [SerializeField] float interval = 3.0f;
+    //消えていく時間
+    [SerializeField] float deleteTime = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time < interval)
+        {
+            foot.color = new Color(1, 1, 1, 0.75f);
+        }
+        else if(time < deleteTime)
+        {
+            foot.color = Color.Lerp(new Color(1, 1, 1, 0.75f), new Color(1, 1, 1, 0), time-interval);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ GatherGame/Assets/Memberfile/Yagi/Script/Main/FootPrint/WalkFoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*足跡を表示するプログラム*/
+
+public class WalkFoot : MonoBehaviour
+{
+    public GameObject footPrintPrefab;
+    float time = 0;
+    [SerializeField] float interval = 0.35f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            time += Time.deltaTime;
+            if (time > interval)
+            {
+                time = 0;
+                Instantiate(footPrintPrefab, transform.position, transform.rotation);
+            }
+        }
+    }
+}
