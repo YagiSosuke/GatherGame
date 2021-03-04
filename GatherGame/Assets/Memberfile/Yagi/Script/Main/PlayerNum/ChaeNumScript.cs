c85849765486687e0a963196873c063e992b961a
yagikun <yagikundayo@gmail.com>
2021-03-04 17:42:01 +0900

    ゲーム画面 - 頭上の数字ひとまず完成?表示バグを直す

--- GatherGame/Assets/Memberfile/Yagi/Script/Main/PlayerNum/ChaeNumScript.cs
+++ GatherGame/Assets/Memberfile/Yagi/Script/Main/PlayerNum/ChaeNumScript.cs
@@ -13,8 +13,9 @@ public class ChaeNumScript : MonoBehaviourPunCallbacks, IPunObservable
 
     [SerializeField] Color[] TextColor;
 
-    [SerializeField] int id;
-    bool NumSetF = false;
+    int id;
+
+    [SerializeField] float count = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -26,8 +27,30 @@ public class ChaeNumScript : MonoBehaviourPunCallbacks, IPunObservable
         {
             id = PhotonNetwork.PlayerList.Length;
             NumText.text = id + "P";
-            line1.effectColor = TextColor[id];
+            line1.effectColor = TextColor[id - 1];
         }        
+    }
+
+    void Update() {
+        if (photonView.IsMine)
+        {
+            count += Time.deltaTime;
+
+            if (count < 1.0f)
+            {
+                NumText.fontSize = 50;
+                NumText.text = id + "P";
+            }
+            else if (count < 2.0f)
+            {
+                NumText.fontSize = 35;
+                NumText.text = "あなた";
+            }
+            else
+            {
+                count = 0;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -39,21 +62,17 @@ public class ChaeNumScript : MonoBehaviourPunCallbacks, IPunObservable
     //データを送受信するメソッド
     void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if (!NumSetF)
+        if (stream.IsWriting)
         {
-            if (stream.IsWriting)
-            {
-                //送信側
-                stream.SendNext(id);
-            }
-            else
-            {
-                //受信側
-                id = (int)stream.ReceiveNext();
-                NumText.text = id + "P";
-                line1.effectColor = TextColor[id + 1];
-            }
-            Debug.Log("id = " + id);
+            //送信側
+            stream.SendNext(id);
+        }
+        else
+        {
+            //受信側
+            id = (int)stream.ReceiveNext();
+            NumText.text = id + "P";
+            line1.effectColor = TextColor[id - 1];
         }
     }
 }
